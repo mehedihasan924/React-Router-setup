2bf8dbf28105f7a2e52445328f411cbf1088f8ae
Mehedi-Hasan <mdhasanmehedi924@gmail.com>
2023-09-24 00:12:12 -0700

    Router basic setup

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Outlet} from 'react-router-dom'
+import Header from '../Header/Header';
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h1> This is Home Page</h1>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Home;
