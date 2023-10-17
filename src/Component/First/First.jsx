76e061a4318f68f1bb2e780b9a90b053bf744f1b
Mehedi-Hasan <mdhasanmehedi924@gmail.com>
2023-10-17 04:30:46 -0700

    new

--- src/Component/First/First.jsx
+++ src/Component/First/First.jsx
@@ -7,6 +7,7 @@ const First = () => {
         <div>
              <h1> This is Home page</h1>
              <h1> My Name  is mehedi hasan</h1>
+             <h1> This is routing Project</h1>
              <div className="card">
             <Editor value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
         </div>
